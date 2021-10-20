9489b66a75cb718008997b69930bd08fbdf379f3
wakametarou <wakame.programing@gmail.com>
2021-10-20 19:52:39 +0900

    start.sh-インデント

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ "${RAILS_ENV}" = "production"]
+if [ "${RAILS_ENV}" = "production" ]
 then
-  bundle exec rails assets:precompile
+    bundle exec rails assets:precompile
 fi
 
 bundle exec rails s -p ${PORT:-3000} -b 0.0.0.0
